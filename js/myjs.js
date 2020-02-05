d67691ead189725e9d74a315dbaf25623a1cb50a
osamamousa204 <osamamousa204@gmail.com>
2020-02-05 13:06:46 +0200

    f

--- js/myjs.js
+++ js/myjs.js
@@ -22,17 +22,5 @@ var att = prompt("what is your  attendance grade ?");
 console.log(att);
 var pro = prompt("what is your  professionalism grade?");
 console.log(pro);
-var result ;
 
-if (att >= 90 && grade+pro <= 90) {     
-    result = 'congrasulations you passed to 201 class! :)';   
-} 
-  
-  
-  
- else {     
-    result = 'sorry you droped :(!';   
-} 
-console.log(result);
 
-document.write('<h3>'+result+'</h3>');
