3f52bb679ad99a5837043fad01378961f75af5f2
osamamousa204 <osamamousa204@gmail.com>
2020-02-06 10:39:32 +0200

    g

--- js/myjs.js
+++ js/myjs.js
@@ -8,31 +8,71 @@
 
 
 
-function candivide4 ( ){
-    var N4 =prompt("type a Num can be divided at 4") ;
-    var result;
+// function candivide4 ( ){
+//     var N4 =prompt("type a Num can be divided at 4") ;
+//     var result;
     
-    if (N4 % 4 === 0) {     
-        result =  N4+' is correct';   
-    } 
+//     if (N4 % 4 === 0) {     
+//         result =  N4+' is correct';   
+//     } 
     
       
-     else {     
-        while(N4 %4 !== 0){
-            N4= prompt("type a correct value") ;
-        }
-        result =  N4+' is correct'; 
-    } 
-   return result; 
+//      else {     
+//         while(N4 %4 !== 0){
+//             N4= prompt("type a correct value") ;
+//         }
+//         result =  N4+' is correct'; 
+//     } 
+//    return result; 
 
-}
-var birds = prompt("how many birds do you want?");
-var pic = "";
-function callbirds ( ){
-    for(var i = 0; i < birds; i = i+1){
-        pic = pic + '<img scr="pic/Annotation 2020-02-03 210312.png">';
-    } 
-    return pic;
-}
+// }
+// var birds = prompt("how many birds do you want?");
+// var pic = "";
+// function callbirds ( ){
+//     for(var i = 0; i < birds; i = i+1){
+//         pic = pic + '<img scr="pic/Annotation 2020-02-03 210312.png">';
+//     } 
+//     return pic;
+// }
+function createven  () {
 
+    var N1 = parseInt( prompt("type an even number ?") );
+    console.log(typeof(N1));
+    var result;
+    // console.log(N2);
+    var evenN;
+    console.log(evenN);
+    var result = "" ;
+
+    
+    // var greeting; 
+    
+
+    while (N1  % 2 !== 0 ){
+        N1 = prompt("type a correct value")
+     }
+
+      N2 = prompt("How many lines do you want to print?")
+    
+     for (var i=0;i<N2;i++) {
+        
+            result = result + N1 + '<br/>';
+            
+     }
+     return result;
+    }
+    // if (age > 0 && age<8   ) {     
+    //     greeting = 'children clothes site';   
+    // } 
+    // else if (age >= 8 && age <=18) {     
+    //     greeting = 'ِYoung clothes site';   
+    // } else if (age > 18 && age <=100) {     
+    //     greeting = 'Adult clothes site';   
+    // } else {     
+    //     greeting = 'Something went wrong!';   
+    // } 
+//     return picture;
+// }
+
+document.write('<div class="n">'+createven()+'</div>');
 
